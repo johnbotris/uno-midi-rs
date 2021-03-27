94f7526f89c1e87606f7553fcee162f6d436a897
John <me@johnbotr.is>
2021-03-27 23:08:19 +0100

    enable interrupts so millis works
    also some other stuff

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 #![no_std]
 #![no_main]
+// required by millis
+#![feature(abi_avr_interrupt)]
+
+mod maths;
+mod millis;
+mod patterns;
 
 extern crate panic_halt;
+extern crate ufmt;
+
 use arduino_uno::hal::port::mode::Output;
 use arduino_uno::hal::port::portb::PB5;
 use arduino_uno::prelude::*;
 use arduino_uno::{delay_ms, Peripherals, Pins, Serial};
 
+use millis::*;
+
+const DEFAULT_BAUD_RATE: u32 = 9600;
+const MIDI_BAUD_RATE: u32 = 31250;
+
 #[arduino_uno::entry]
 fn main() -> ! {
     let dp = Peripherals::take().unwrap();
 
-    let mut pins = Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
+    millis_init(dp.TC0);
 
-    // let mut midi = pins.d1.into_output(&mut pins.ddr);
+    let mut pins = Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
 
     let mut serial = Serial::new(
         dp.USART0,
         pins.d0,
         pins.d1.into_output(&mut pins.ddr),
-        31250.into_baudrate(),
+        MIDI_BAUD_RATE.into_baudrate(),
     );
 
     let mut note_on = |pitch, velocity| {
@@ -28,11 +41,23 @@ fn main() -> ! {
         serial.write_byte(velocity);
     };
 
-    loop {
-        note_on(0x1e, 0x45);
-        delay_ms(100u16);
+    let bpm = 120.0;
 
-        note_on(0x1e, 0x00);
-        delay_ms(100u16);
+    let framerate = 1;
+    let ms_per_frame = 1000 / framerate;
+    let mut frame_start = millis();
+
+    let mut is_on = true;
+
+    loop {
+        let now = millis();
+
+        // ufmt::uwriteln!(&mut serial, "now = {}\r", now).void_unwrap();
+        if now - frame_start >= ms_per_frame {
+            let velocity = is_on as u8 * 0x45;
+            is_on = !is_on;
+            frame_start = now;
+            note_on(32, velocity);
+        }
     }
 }
